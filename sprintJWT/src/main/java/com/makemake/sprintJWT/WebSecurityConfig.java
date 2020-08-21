06551e7271ce3f91e09df5aeb40ac19e446aecbe
RyanGao67 <tiangao0611@gmail.com>
2020-08-21 16:36:05 -0400

    a

---
+++ sprintJWT/src/main/java/com/makemake/sprintJWT/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.makemake.sprintJWT;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity  // enable spring security's (web security support) and provide (the spring MVC integration)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+// in this method / and /home paths are configured to not require any authentication
+    // all other paths must be authenticated
+    // when a user successfully logs in, they are redirected to the previously requested page that required authentication
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
